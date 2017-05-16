1f351d1d73e883f3837763cb9b50bb64c70ab2c9
natimendezb <nati.mendezb@gmail.com>
2017-05-16 21:16:09 +0200

    Actualización JS y CSS
    JS: agregado de cambio de header de acuerdo al scroll.
    CSS: mejoras estilo Formación y agregado de división entre Conocimientos
    y Formación.

--- js/acciones.js
+++ js/acciones.js
@@ -1,3 +1,29 @@
-function cambio_titulo(nuevo_texto) {
-    document.getElementById("titulo_seccion").innerHTML = nuevo_texto; 
+function cambio_titulo_onclick(nuevo_texto) {
+    var pDelHeader = document.getElementById("titulo_seccion");
+    
+    pDelHeader.innerHTML = nuevo_texto; 
+}
+
+function cambio_titulo_onscroll() {
+    var pixelesScroll = document.body.scrollTop;
+    var botonPerfil = document.getElementById("boton_perfil");
+        botonPerfil.style.opacity = "1";
+    var botonConocimientos = document.getElementById("boton_conocimientos");
+        botonConocimientos.style.opacity = "1";
+    var botonProyectos = document.getElementById("boton_proyectos");
+        botonProyectos.style.opacity = "1";
+    
+    if (pixelesScroll <= 1000) {
+        cambio_titulo_onclick("PERFIL");
+        botonPerfil.style.opacity = "0.5";
+    }
+    if (pixelesScroll > 1000 && pixelesScroll <= 3300) {
+        cambio_titulo_onclick("CONOCIMIENTOS");
+        botonConocimientos.style.opacity = "0.5";
+        //document.getElementById("conocimientos").scrollIntoView();
+    }
+    if (pixelesScroll > 3300) {
+        cambio_titulo_onclick("PROYECTOS");
+        botonProyectos.style.opacity = "0.5";
+    }
 }
