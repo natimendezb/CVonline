1a28364f02fd6a61aae75d926ba3e12e8421d172
natimendezb <nati.mendezb@gmail.com>
2017-06-02 16:53:45 +0200

    Proyectos con cartas diseñadas
    Dejé la animación que había encontrado, pero le saqué el onclick que
    hicimos nosotros. Las cartas ya tienen los dos lados diseñados.

--- js/acciones.js
+++ js/acciones.js
@@ -49,11 +49,11 @@ function calculateAge() {
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-function muestra_datos_extra(idDeInfoOculta){
-    var textoOculto = document.getElementById(idDeInfoOculta);
-    
-    textoOculto.style.visibility = "visible";
-    setTimeout(
-        function () { document.getElementById(idDeInfoOculta).style.visibility = "hidden";}, 5000
-    );
-}
+//function muestra_datos_extra(idDeInfoOculta){
+//    var textoOculto = document.getElementById(idDeInfoOculta);
+//    
+//    textoOculto.style.visibility = "visible";
+//    setTimeout(
+//        function () { document.getElementById(idDeInfoOculta).style.visibility = "hidden";}, 5000
+//    );
+//}
