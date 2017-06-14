c030a9a58a1b5bf1a49d64afa0fb276ae86b87c9
natimendezb <nati.mendezb@gmail.com>
2017-06-14 20:54:17 +0200

    Actualización JS desktop

--- js/acciones-desktop.js
+++ js/acciones-desktop.js
@@ -1,6 +1,5 @@
 function cambio_titulo_onscroll() {
     var pixelesScroll = document.body.scrollTop;
-    posicionActual = pixelesScroll;
     var botonPerfil = document.getElementById("boton_perfil");
         botonPerfil.style.opacity = "1";
     var botonConocimientos = document.getElementById("boton_conocimientos");
@@ -12,34 +11,27 @@ function cambio_titulo_onscroll() {
     
     var pixelesPerfil, pixelesConocimientos, pixelesProyectos
     
-    if (esPortrait()) {
-        pixelesPerfil = 1200;
-        pixelesConocimientos = 3800;
-        pixelesProyectos = 8000;
-    }
-    else {
-        pixelesPerfil = 600;
-        pixelesConocimientos = 2000;
-        pixelesProyectos = 5500;
-    }
+    pixelesPerfil = 450;
+    pixelesConocimientos = 1500;
+    pixelesProyectos = 3800;
     
     if (pixelesScroll <= pixelesPerfil) {
-            cambio_titulo_onclick("PERFIL");
-            botonPerfil.style.opacity = "0.5";
-        }
-        if (pixelesScroll > pixelesPerfil && pixelesScroll <= pixelesConocimientos) {
-            cambio_titulo_onclick("CONOCIMIENTOS");
-            botonConocimientos.style.opacity = "0.5";
-        }
-        if (pixelesScroll > pixelesConocimientos && pixelesScroll <= pixelesProyectos) {
-            cambio_titulo_onclick("PROYECTOS");
-            botonProyectos.style.opacity = "0.5";
-        }
+        cambio_titulo_onclick("PERFIL");
+        botonPerfil.style.opacity = "0.5";
+    }
+    if (pixelesScroll > pixelesPerfil && pixelesScroll <= pixelesConocimientos) {
+        cambio_titulo_onclick("CONOCIMIENTOS");
+        botonConocimientos.style.opacity = "0.5";
+    }
+    if (pixelesScroll > pixelesConocimientos && pixelesScroll <= pixelesProyectos) {
+        cambio_titulo_onclick("PROYECTOS");
+        botonProyectos.style.opacity = "0.5";
+    }
 
-        if (pixelesScroll > pixelesProyectos) {
-            cambio_titulo_onclick("PORTFOLIO");
-            botonPortfolio.style.opacity = "0.5";
-        }
+    if (pixelesScroll > pixelesProyectos) {
+        cambio_titulo_onclick("PORTFOLIO");
+        botonPortfolio.style.opacity = "0.5";
+    }
     
 }
 
@@ -75,6 +67,46 @@ $(document).ready(function(){
         dots: true,
         arrows: false
     });
+    
+    var figuresPortfolio = document.getElementsByClassName("portfolio_proyectos");
+
+    Array.prototype.forEach.call(figuresPortfolio,
+        function(element) {
+            var imagen_portfolio = element.children[0].children[0];
+            var titulo_portfolio = element.children[1].children[0];
+            imagen_portfolio.addEventListener('mouseover',
+                function() {
+                    imagen_portfolio.style.opacity = '0.6';
+                    titulo_portfolio.style.textDecoration = "underline";
+                }
+            );
+            element.addEventListener('mouseout',
+                function() {
+                    imagen_portfolio.style.opacity = '1';
+                    titulo_portfolio.style.textDecoration = 'none';
+                }
+            );
+        }
+    )
+
+    Array.prototype.forEach.call(figuresPortfolio,
+        function(element) {
+            var imagen_portfolio = element.children[0].children[0];
+            var titulo_portfolio = element.children[1].children[0];
+            titulo_portfolio.addEventListener('mouseover',
+                function() {
+                    imagen_portfolio.style.opacity = '0.6';
+                    titulo_portfolio.style.textDecoration = "underline";
+                }
+            );
+            element.addEventListener('mouseout',
+                function() {
+                    imagen_portfolio.style.opacity = '1';
+                    titulo_portfolio.style.textDecoration = 'none';
+                }
+            );
+        }
+    )
 });
 
 
